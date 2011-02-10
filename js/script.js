26954d8f4cb62384e079235d0e288da486781d98
mhamrah <mhamrah@gmail.com>
2011-02-10 08:15:21 -0500

    Starting from html boilerplate
    Signed-off-by: mhamrah <mhamrah@gmail.com>

---
+++ js/script.js
@@ -0,0 +1,34 @@
+/* Author: 
+
+*/
+
+$(document).ready(function () {
+  $.getJSON('http://api.flickr.com/services/feeds/photos_public.gne?id=73079712@N00&tags=hdr,alaska,new zealand,favorite&tagmode=any&format=json&jsoncallback=?', showImages);
+  
+
+});
+
+function showImages(data)
+{
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
