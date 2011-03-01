d3469d1a3501054bceee03234b3c523aba026022
mhamrah <mhamrah@gmail.com>
2011-03-01 12:58:37 -0500

    Upping html5boilerplate to latest version

--- js/script.js
+++ js/script.js
@@ -2,15 +2,6 @@
 
 */
 
-$(document).ready(function () {
-  $.getJSON('http://api.flickr.com/services/feeds/photos_public.gne?id=73079712@N00&tags=hdr,alaska,new zealand,favorite&tagmode=any&format=json&jsoncallback=?', showImages);
-  
-
-});
-
-function showImages(data)
-{
-}
 
 
 
@@ -31,4 +22,17 @@ function showImages(data)
 
 
 
+
+
+
+// usage: log('inside coolFunc', this, arguments);
+// paulirish.com/2009/log-a-lightweight-wrapper-for-consolelog/
+function log(){
+  log.history = log.history || [];   // store logs to an array for reference
+  log.history.push(arguments);
+  arguments.callee = arguments.callee.caller;  
+  if(this.console) console.log( Array.prototype.slice.call(arguments) );
+};
+// make it safe to use console.log always
+(function(b){function c(){}for(var d="assert,count,debug,dir,dirxml,error,exception,group,groupCollapsed,groupEnd,info,log,markTimeline,profile,profileEnd,time,timeEnd,trace,warn".split(","),a;a=d.pop();)b[a]=b[a]||c})(window.console=window.console||{});
 
